0e63716941cc949fe4e9fcd6f15a3dffcc1a6a24
Tom Li <biergaizi2009@gmail.com>
2013-11-30 14:01:20 +0800

    Update macros

--- bits.h
+++ bits.h
@@ -11,9 +11,9 @@ typedef char byte;
 
 #define SIZE_IN_BITS(i) (sizeof(i) * 8)
 #define SIZE(i) ((i) / SIZE_IN_BITS(bit_array) == 0 ? 1 : (i) / SIZE_IN_BITS(bit_array))
-#define BIT_SET(a,x) ((a) |= (1<<(x)))
-#define BIT_CLEAR(x,n) (x = x&~(0x01<<n))
-#define BIT_CHECK(a,x) (((a) & (1<<(x))) != 0)
+#define BIT_SET(a,x) ((a) |= (1 << (x)))
+#define BIT_CLEAR(a,x) ((a) &= ~(1 << (x)))
+#define BIT_CHECK(a,x) (((a) & (1 << (x))) != 0)
 
 
 bit get_bit(bit_array *bits, size_t index);
