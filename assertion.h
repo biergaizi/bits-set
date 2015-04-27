647d265051cddca945e9c7a37916f1ed1112b6f2
Tom Li <biergaizi2009@gmail.com>
2015-04-27 22:35:11 +0800

    assertion.h: add brackets to expr to fix priority
    A good catch from GCC 5.1.0.

--- assertion.h
+++ assertion.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 #define assert(expr) { \
-    if (!expr) { \
+    if (!(expr)) { \
         fprintf(stderr, "%s:%d: Assertion `%s` failed.\n", __FILE__, __LINE__, __STRING(expr)); \
     } \
 }
